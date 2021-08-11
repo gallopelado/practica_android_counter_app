abd33d3f656d63e0f8a602be65dc4be86d66d0d4
Juan <juanftp100@gmail.com>
2021-08-10 22:12:18 -0400

    add: empezar a desarrollar en el MainActivity usando viewBinding

--- app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
@@ -2,10 +2,23 @@ package com.cursosandroidant.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cursosandroidant.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCount()
+    }
+
+    private fun setCount() {
+        binding.tvCount.text = count.toString()
     }
 }
