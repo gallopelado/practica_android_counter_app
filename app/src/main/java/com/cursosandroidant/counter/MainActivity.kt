d224995c2cb5d5684ea66c8949a2d2da9b9542ce
Juan <juanftp100@gmail.com>
2021-08-15 15:05:29 -0400

    add: buena practica añadida y prueba del ciclo de vida entendo desde los estados al mover la pantalla

--- app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
@@ -2,6 +2,8 @@ package com.cursosandroidant.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.cursosandroidant.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Log.i("Lifecycle:", "onCreate")
         setCount()
 
         binding.btnSum.setOnClickListener {
@@ -29,6 +31,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /* Ciclo de vida, analizar*/
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle:","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle:", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle:", "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle:","onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle:","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "onDestroy")
+    }
+
     private fun setCount() {
         binding.tvCount.text = count.toString()
     }
@@ -36,15 +69,19 @@ class MainActivity : AppCompatActivity() {
     // Está especializado en guardar o retener el valor, dentro de la vista
     override fun onSaveInstanceState(outState: Bundle) {
         outState.run {
-            putInt("param_count", count)
+            putInt(Companion.PARAM_COUNT, count)
         }
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        count = savedInstanceState.getInt("param_count")
+        count = savedInstanceState.getInt(Companion.PARAM_COUNT)
         setCount()
         super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    companion object {
+        private const val PARAM_COUNT: String = "param_count"
     }
 
 }
