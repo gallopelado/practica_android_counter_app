bccfcb9ece45437a9c7eaf548319bcc12ed25327
Juan <juanftp100@gmail.com>
2021-08-15 14:53:33 -0400

    add: guardar el estado de lata con onveInstanceState y restaurar con onRestoreInstanceState

--- app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/counter/MainActivity.kt
@@ -32,4 +32,19 @@ class MainActivity : AppCompatActivity() {
     private fun setCount() {
         binding.tvCount.text = count.toString()
     }
+
+    // Está especializado en guardar o retener el valor, dentro de la vista
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putInt("param_count", count)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        count = savedInstanceState.getInt("param_count")
+        setCount()
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
 }
